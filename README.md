b506eba9280624caee68b1d8c7fbc62e28ce1ee3
traplab <roocahy@gmail.com>
2020-03-25 12:00:41 +0300

    install ffmpeg

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
+Author:traplab
+
 # BatchMp3converter
-implement ffmpeg to convert a batch of files
+implement ffmpeg(first installed by script) to convert a batch of files
+,,,,,,
+Tools
+``````
+ffmpeg
+
+,,,,,,
+usage
+``````
+bash <scriptName>
 
 The scripts input is in path from
 Working on module for receiving user textfile containing relativePaths
--- ffmpegConverter.sh
+++ ffmpegConverter.sh
@@ -1,19 +1,42 @@
 #!/bin/bash
 
-printf "%+35s %-6s %-4s\n"             ----- ----- -----
-printf  "%+31s %14s %-1s\n" '|' 'Bulk converter'  '|'
-printf  "%+31s %-14s %-1s\n" '|'  'with ffmpeg'    '|'
-printf "%+35s %-6s %-4s\n" ------ ---- -----
+printf "%+35s %-8s %-4s\n"             ----- ----- -----
+printf  "%+31s %18s %-1s\n" '|' 'Batch Mp3converter'  '|'
+printf  "%+31s %-18s %-1s\n" '|'  ' @traplab'    '|'
+printf "%+35s %-8s %-4s\n" ----- ---- -----
 
-read -p "Continue ? " answer
+
 cat << here
 options: y/Y--continue to converter
           ?--continue to advancedMode(enRoute)
+          n/N--quit
+here
 
+read -p "Continue ? " answer
+echo ""
 defaultTextFilePath=./convert.txt
 
 if [ $answer = "y" -o $answer = "Y" ]
 then
+#check for net
+cat << here
+install ffmpeg
+here
+
+for package in ffmpeg ;
+do dpkg -s "$package" > /dev/null 2>&1 &&  echo "$package is installed"  ||
+if ping -c 2 -q -W 3 google.com > /dev/null 2>&1 ;
+then
+echo "Downloading packages" && if [ "$EUID" -ne 0 ] ;
+then sudo apt-get install $package
+else
+apt-get install $package
+fi
+else
+echo "My guy, hauna net.This is as far as  we can go"
+exit
+fi
+done
 
 TextFilePath=$defaultTextFilePath
 
@@ -33,10 +56,16 @@ convertLine=$(echo $New_LINE | cut -f1 -d ".")
 #echo "$convertLine"
 echo -e  $filePath\"$New_LINE\" $filePath2\"$convertLine.mp3\" |xargs ffmpeg -i 
 done < $TextFilePath
+elif [ $answer = "n" -o $answer = "N" ]
+then 
+exit
+elif [ $answer = "?" ]
+then echo "chill"
 else
+echo "wrong input"
 exit
 fi
-|'#what I wanted was to include an instance wherby the user has a file
+: '#what I wanted was to include an instance wherby the user has a file
 # list of relative paths to files to convert,for now append or recreate.Simple.haha
 elif [ $answer = "?" ]
 
